21a2f02306497b7025148094725c7727166d0979
Joel Dice <joel.dice@gmail.com>
2012-07-27 16:32:23 -0600

    add license.txt and copyright headers

--- src/com/readytalk/revori/imp/MyRevision.java
+++ src/com/readytalk/revori/imp/MyRevision.java
@@ -1,3 +1,10 @@
+/* Copyright (c) 2010-2012, Revori Contributors
+
+   Permission to use, copy, modify, and/or distribute this software
+   for any purpose with or without fee is hereby granted, provided
+   that the above copyright notice and this permission notice appear
+   in all copies. */
+
 package com.readytalk.revori.imp;
 
 import static com.readytalk.revori.util.Util.copy;
