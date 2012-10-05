ba7f8e3e3411a99d26f79ad628723028bd569fce
Joel Dice <joel.dice@gmail.com>
2012-10-05 15:53:29 -0600

    add Servers class

---
+++ src/com/readytalk/revori/server/Servers.java
@@ -0,0 +1,135 @@
+/* Copyright (c) 2010-2012, Revori Contributors
+
+   Permission to use, copy, modify, and/or distribute this software
+   for any purpose with or without fee is hereby granted, provided
+   that the above copyright notice and this permission notice appear
+   in all copies. */
+
+package com.readytalk.revori.server;
+
+import com.readytalk.revori.Revision;
+import com.readytalk.revori.ConflictResolver;
+import com.readytalk.revori.ForeignKeyResolver;
+import com.readytalk.revori.subscribe.Subscription;
+import com.readytalk.revori.server.protocol.Readable;
+
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Servers {
+  public static RevisionServer asynchronousRevisionServer
+    (final RevisionServer server,
+     ConflictResolver conflictResolver,
+     ForeignKeyResolver foreignKeyResolver,
+     final TaskHandler handler)
+  {
+    return new AsynchronousRevisionServer
+      (server, conflictResolver, foreignKeyResolver, handler);
+  }
+
+  public static NetworkServer asynchronousNetworkServer
+    (final NetworkServer server,
+     ConflictResolver conflictResolver,
+     ForeignKeyResolver foreignKeyResolver,
+     final TaskHandler handler)
+  {
+    return new AsynchronousNetworkServer
+      (server, conflictResolver, foreignKeyResolver, handler);
+  }
+
+  private static class AsynchronousRevisionServer implements RevisionServer {
+    protected final RevisionServer server;
+    private final ConflictResolver conflictResolver;
+    private final ForeignKeyResolver foreignKeyResolver;
+    private final List<Runnable> listeners = new ArrayList();
+    private Revision head;
+    private Revision base;
+
+    public AsynchronousRevisionServer
+      (final RevisionServer server,
+       final ConflictResolver conflictResolver,
+       final ForeignKeyResolver foreignKeyResolver,
+       final TaskHandler handler)
+    {
+      this.server = server;
+      this.conflictResolver = conflictResolver;
+      this.foreignKeyResolver = foreignKeyResolver;
+
+      head = base = server.head();
+
+      server.registerListener(new Runnable() {
+          public void run() {
+            handler.handleTask(new Runnable() {
+                public void run() {
+                  Revision fork = server.head();
+                  if (base != fork || base != head) {
+                    head = base = base.merge
+                      (head, fork, conflictResolver, foreignKeyResolver);
+
+                    for (Runnable listener: listeners) {
+                      listener.run();
+                    }
+                  }
+                }
+              });
+          }
+        });
+    }
+
+    public Revision head() {
+      return head;
+    }
+
+    public void merge(Revision base, Revision fork) {
+      if (base != fork || base != head) {
+        head = base.merge(head, fork, conflictResolver, foreignKeyResolver);
+
+        server.merge(this.base, head);
+
+        for (Runnable listener: listeners) {
+          listener.run();
+        }
+      }
+    }
+
+    public Subscription registerListener(final Runnable listener) {
+      listeners.add(listener);
+
+      return new Subscription() {
+        public void cancel() {
+          listeners.remove(listener);
+        }
+      };
+    }
+  }
+
+  private static class AsynchronousNetworkServer
+    extends AsynchronousRevisionServer
+    implements NetworkServer
+  {
+    private final NetworkServer networkServer;
+
+    public AsynchronousNetworkServer(final NetworkServer server,
+                                     ConflictResolver conflictResolver,
+                                     ForeignKeyResolver foreignKeyResolver,
+                                     final TaskHandler handler)
+    {
+      super(server, conflictResolver, foreignKeyResolver, handler);
+
+      this.networkServer = server;
+    }
+
+    public void accept(NodeID source, Readable message) {
+      networkServer.accept(source, message);
+    }
+
+    public void updateView(Set<NodeID> directlyConnectedNodes) {
+      networkServer.updateView(directlyConnectedNodes);
+    }
+  }
+
+  public interface TaskHandler {
+    public void handleTask(Runnable task);
+  }
+}
---
+++ test/unittests/Asynchronous.java
@@ -0,0 +1,124 @@
+/* Copyright (c) 2010-2012, Revori Contributors
+
+   Permission to use, copy, modify, and/or distribute this software
+   for any purpose with or without fee is hereby granted, provided
+   that the above copyright notice and this permission notice appear
+   in all copies. */
+
+package unittests;
+
+import static com.readytalk.revori.util.Util.cols;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+import com.readytalk.revori.Revision;
+import com.readytalk.revori.Table;
+import com.readytalk.revori.Column;
+import com.readytalk.revori.ConflictResolvers;
+import com.readytalk.revori.ForeignKeyResolvers;
+import com.readytalk.revori.server.RevisionServer;
+import com.readytalk.revori.server.SimpleRevisionServer;
+import com.readytalk.revori.server.Servers;
+import com.readytalk.revori.server.Servers.TaskHandler;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Asynchronous extends TestCase {
+  @Test
+  public void test() {
+    RevisionServer rawServer = new SimpleRevisionServer
+      (ConflictResolvers.Restrict, ForeignKeyResolvers.Restrict);
+
+    MyTaskHandler handler = new MyTaskHandler();
+
+    RevisionServer asyncServer = Servers.asynchronousRevisionServer
+      (rawServer, ConflictResolvers.Restrict, ForeignKeyResolvers.Restrict,
+       handler);
+
+    Column<Integer> number = new Column<Integer>(Integer.class);
+    Column<String> name = new Column<String>(String.class);
+    Table numbers = new Table(cols(number));
+
+    Revision base = rawServer.head();
+    rawServer.merge(base, base.builder().table(numbers).row(11)
+                    .update(name, "once").commit());
+
+    assertEquals("once",
+                 rawServer.head().query(name, numbers.primaryKey, 11));
+
+    assertEquals(null,
+                 asyncServer.head().query(name, numbers.primaryKey, 11));
+
+    handler.flush();
+
+    assertEquals("once",
+                 rawServer.head().query(name, numbers.primaryKey, 11));
+    
+    assertEquals("once",
+                 asyncServer.head().query(name, numbers.primaryKey, 11));
+
+    base = asyncServer.head();
+    asyncServer.merge(base, base.builder().table(numbers).row(12)
+                      .update(name, "doce").commit());
+
+    base = rawServer.head();
+    rawServer.merge(base, base.builder().table(numbers).row(10)
+                    .update(name, "diez").commit());
+
+    assertEquals("diez",
+                 rawServer.head().query(name, numbers.primaryKey, 10));
+
+    assertEquals(null,
+                 asyncServer.head().query(name, numbers.primaryKey, 10));
+
+    assertEquals("once",
+                 rawServer.head().query(name, numbers.primaryKey, 11));
+    
+    assertEquals("once",
+                 asyncServer.head().query(name, numbers.primaryKey, 11));
+
+    assertEquals("doce",
+                 rawServer.head().query(name, numbers.primaryKey, 12));
+    
+    assertEquals("doce",
+                 asyncServer.head().query(name, numbers.primaryKey, 12));
+
+    handler.flush();
+
+    assertEquals("diez",
+                 rawServer.head().query(name, numbers.primaryKey, 10));
+
+    assertEquals("diez",
+                 asyncServer.head().query(name, numbers.primaryKey, 10));
+
+    assertEquals("once",
+                 rawServer.head().query(name, numbers.primaryKey, 11));
+    
+    assertEquals("once",
+                 asyncServer.head().query(name, numbers.primaryKey, 11));
+
+    assertEquals("doce",
+                 rawServer.head().query(name, numbers.primaryKey, 12));
+    
+    assertEquals("doce",
+                 asyncServer.head().query(name, numbers.primaryKey, 12));
+  }
+
+  private static class MyTaskHandler implements TaskHandler {
+    public final List<Runnable> tasks = new ArrayList();
+
+    public void handleTask(Runnable task) {
+      tasks.add(task);
+    }
+
+    public void flush() {
+      for (Runnable task: tasks) {
+        task.run();
+      }
+      tasks.clear();
+    }
+  }
+}
